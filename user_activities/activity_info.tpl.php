95188f989e70e67ccab71472e1a52317b9502602
Roman Barbun <roman@telegraafnet.nl>
2015-01-13 08:19:11 +0100

    Custom module to display user's recent activity

---
+++ user_activities/activity_info.tpl.php
@@ -0,0 +1,3 @@
+<?php print $link; ?>
+<?php print $body; ?> <?php print $view_link; ?>
+<p class="activity-date"><?php print $date; ?></p>
---
+++ user_activities/activity_user_info.tpl.php
@@ -0,0 +1,9 @@
+<?php print $userpic; ?>
+<div class="user-info">
+  <h2 class="user-info-username"><?php print $user->name; ?></h2>
+  <p class="user-info-comments-count"><?php print $comment_count; ?></p>
+  <p class="iser-info-nodes-count"><?php print $node_count; ?></p>
+</div>
+<?php print $profile_link; ?>
+<?php print $ignored_link; ?>
+<?php print $profile_ips; ?>
